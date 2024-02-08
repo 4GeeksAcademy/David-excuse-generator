d0f947c691c6a7ec80d9e8050b047798c2566f6f
David-Caracas <153639564+David-Caracas@users.noreply.github.com>
2024-02-08 01:33:50 +0000

    excuse-generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function randomGuess(array) {
+  let guess;
+  guess = array[Math.floor(Math.random() * array.length)];
+  return guess;
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function excuseGenerator(who, action, what, when) {
+  let excuse = "";
+  excuse =
+    randomGuess(who) +
+    " " +
+    randomGuess(action) +
+    " " +
+    randomGuess(what) +
+    " " +
+    randomGuess(when) +
+    ".";
+  return excuse;
+}
+let myExcuse = excuseGenerator(who, action, what, when);
+
+document.getElementById("excuse").innerHTML = myExcuse;
